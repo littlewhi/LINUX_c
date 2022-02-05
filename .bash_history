de3b82f7f545811d822a2621d7e7b582176f0ec0
yang <root@localhost.localdomain>
2022-02-04 16:23:36 -0800

    2-5

--- .bash_history
+++ .bash_history
@@ -24,3 +24,35 @@ sudo -s
 gcc version
 gcc --version
 man gcc
+sudo -s
+gcc --version
+exit
+apt-get install fcitx
+vim
+remove -rm test.c
+rm remoce test.c
+rm test.c
+rm test1.c
+rm a.out
+yum install fcitx
+sudo -s
+exit
+sudo -s
+touch p
+vim p
+vim test.c
+gcc test.c
+./a.out
+vim test.c
+ls
+rm a.out
+rm c-language-1
+ls sudo -s
+sudo -s
+vim test.c
+gcc test.c
+./a.out
+vim test.c
+gcc test.c
+./a.out
+sudo -s
--- test.c
+++ test.c
@@ -1,116 +1,66 @@
-int removint removefun(int* nums,int numsSize)
+#include<stdio.h>
+
+/*int remov(int* nums, int numsSize, int val)
+{ 
+  int left = 0;
+  int right = numsSize - 1;
+  while( left <= right)
+  {
+    int temp;
+    while((left <= right) && (*(nums + right) == val))
+      right--;
+    while((left <= right) && (*(nums + left) != val))
+      left++;
+    if(left < right)
+    {
+      temp = *(nums + left);
+      *(nums + left) = *(nums + right);
+      *(nums + right) = temp;
+    }
+  }
+  return (left);
+}
+*/
+/*int main()
 {
-	//int ret = numsSize;
-	//	int i = 0;
-	//		for (; i < numsSize - 1; i++)
-	//			{
-	//					if (*(nums + i) == *(nums + i + 1))
-	//							{
-	//										int j = 1;
-	//													int k = 0;
-	//																while (*(nums + i) == *(nums + i + 1 + j) && ((i + 1 + j) < numsSize))
-	//																				j++;
-	//																							while (1 + j + k < numsSize - i)
-	//																										{
-	//																														*(nums + i + 1 + k) = *(nums + i + 1 + j + k);
-	//																																		k++;
-	//																																					}
-	//																																								numsSize -= j;
-	//																																										}
-	//																																											}
-	//																																												return numsSize;
-	//																																												}
-	//
-	//																																												int main()
-	//																																												{
-	//																																													int i = 0;
-	//																																														int arr[] = { 1,1 };
-	//																																															//int arr[] = {0,0,0,1,2,2,2,34,55,55,55,55,67,67,68,89,100,100,101};
-	//																																																int len = sizeof(arr) / sizeof(arr[0]);
-	//																																																	len = removefun(arr,len);
-	//																																																		for (i = 0; i < len; i++)
-	//																																																			{
-	//																																																					printf("%d ", arr[i]);
-	//																																																						}
-	//																																																							return 0;
-	//																																int removefun(int* nums, int numsSize)
-	//																																{
-	//																																	//int ret = numsSize;
-	//																																		int i = 0;
-	//																																			for (; i < numsSize - 1; i++)
-	//																																				{
-	//																																						if (*(nums + i) == *(nums + i + 1))
-	//																																								{
-	//																																											int j = 1;
-	//																																														int k = 0;
-	//																																																	while (*(nums + i) == *(nums + i + 1 + j) && ((i + 1 + j) < numsSize))
-	//																																																					j++;
-	//																																																								while (1 + j + k < numsSize - i)
-	//																																																											{
-	//																																																															*(nums + i + 1 + k) = *(nums + i + 1 + j + k);
-	//																																																																			k++;
-	//																																																																						}
-	//																																																																									numsSize -= j;
-	//																																																																											}
-	//																																																																												}
-	//																																																																													return numsSize;
-	//																																																																													}
-	//
-	//																																																																													int main()
-	//																																																																													{
-	//																																																																														int i = 0;
-	//																																																																															int arr[] = { 1,1 };
-	//																																																																																//int arr[] = {0,0,0,1,2,2,2,34,55,55,55,55,67,67,68,89,100,100,101};
-	//																																																																																	int len = sizeof(arr) / sizeof(arr[0]);
-	//																																																																																		len = removefun(arr,len);
-	//																																																																																			for (i = 0; i < len; i++)
-	//																																																																																				{
-	//																																																																																						printf("%d ", arr[i]);
-	//																																																																																							}
-	//																																																																																								return 0;
-	//																																																																	int removefun(int* nums, int numsSize)
-	//																																																																	{
-	//																																																																		//int ret = numsSize;
-	//																																																																			int i = 0;
-	//																																																																				for (; i < numsSize - 1; i++)
-	//																																																																					{
-	//																																																																							if (*(nums + i) == *(nums + i + 1))
-	//																																																																									{
-	//																																																																												int j = 1;
-	//																																																																															int k = 0;
-	//																																																																																		while (*(nums + i) == *(nums + i + 1 + j) && ((i + 1 + j) < numsSize))
-	//																																																																																						j++;
-	//																																																																																									while (1 + j + k < numsSize - i)
-	//																																																																																												{
-	//																																																																																																*(nums + i + 1 + k) = *(nums + i + 1 + j + k);
-	//																																																																																																				k++;
-	//																																																																																																							}
-	//																																																																																																										numsSize -= j;
-	//																																																																																																												}
-	//																																																																																																													}
-	//																																																																																																														return numsSize;
-	//																																																																																																														}
-	//
-	//																																																																																																														int main()
-	//																																																																																																														{
-	//																																																																																																															int i = 0;
-	//																																																																																																																int arr[] = { 1,1 };
-	//																																																																																																																	//int arr[] = {0,0,0,1,2,2,2,34,55,55,55,55,67,67,68,89,100,100,101};
-	//																																																																																																																		int len = sizeof(arr) / sizeof(arr[0]);
-	//																																																																																																																			len = removefun(arr,len);
-	//																																																																																																																				for (i = 0; i < len; i++)
-	//																																																																																																																					{
-	//																																																																																																																							printf("%d ", arr[i]);
-	//																																																																																																																								}
-	//																																																																																																																									return 0;
-	//																																																																																																																									}Size)
+  int i = 0;
+  int arr[]={0};
+  int len = sizeof(arr) / sizeof(arr[0]);
+  len = remov(arr,len,5);
+  for(i = 0;i < len;i++)
+  {
+    printf("%d ",arr[i]);
+  }
+  printf("\n");
+}*/
+
+int remov(int* nums,int numsSize,int val)
 {
-	//int ret = numsSize;
-	//	int i = 0;
-	//		for (; i < numsSize - 1; i++)
-	//			{
-	//					if (*(nums + i) == *(nums + i + 1))
-	//							{
-	//										int j = 1;
-	//													int k = 0;
-	//																while (*(nums + i) == *(nums + i + 1 + j) && (																																																}}
+  int left = 0;
+  int right = numsSize;
+  while(left < right)
+  {
+    if(*(nums + left) == val)
+    {
+      *(nums+left) = *(nums+right - 1);
+      right--;
+    }
+    else
+      left++;
+  }
+
+}
+
+int main()
+{
+  int i = 0;
+  int arr[]={0};
+  int len = sizeof(arr) / sizeof(arr[0]);
+  len = remov(arr,len,5);
+  for(i = 0;i < len;i++)
+  {
+    printf("%d ",arr[i]);
+  }
+  printf("\n");
+}
+
